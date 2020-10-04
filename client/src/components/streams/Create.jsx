e695c73031e8de3574b7d247659ff1330ae7a214
mdabhn <md.abhn@gmail.com>
2020-10-04 21:00:12 +0600

    REDUX-FORM ADDED WITH VALIDATION CHECK ✔✔✔

--- client/src/components/streams/Create.jsx
+++ client/src/components/streams/Create.jsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const Create = () => {
-  return <div>Lets Create</div>;
+class Create extends Component {
+  renderInputs({ input, label, meta }) {
+    return (
+      <div className='field'>
+        <label htmlFor={label}>{label}</label>
+        <input {...input} autoComplete='off' />
+        {meta.touched ? <div style={{ color: 'red' }}>{meta.error}</div> : null}
+      </div>
+    );
+  }
+
+  onSubmit(e) {
+    console.log(e);
+  }
+
+  render() {
+    return (
+      <form
+        className='ui form'
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
+        <Field name='title' component={this.renderInputs} label='Title' />
+        <Field
+          name='description'
+          component={this.renderInputs}
+          label='Description'
+        />
+
+        <button className='ui button primary'>Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (value) => {
+  const errors = {};
+
+  if (!value.title) errors.title = 'Required';
+  if (!value.description) errors.description = 'Required';
+
+  return errors;
 };
 
-export default Create;
+export default reduxForm({
+  form: 'createNewStream',
+  validate,
+})(Create);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer } from 'redux-form';
 import auth from './authReducer';
 
 export default combineReducers({
   auth: auth,
+  form: reducer,
   default: () => 'default',
 });
